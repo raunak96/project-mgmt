efaf971acc478f03d1e12ae2de3892df4502ceb3
Raunak Kedia <raunakkedia1@gmail.com>
2022-12-18 20:19:53 +0530

    initial commit and Schema added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { trpc } from "../utils/trpc";
+
+import "../styles/globals.css";
+import Head from "next/head";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  const description = "Manage you projects in a simple manner";
+  const title = "Project Mgmt Tool";
+  return (
+    <SessionProvider session={session}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import { type NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { trpc } from "@/utils/trpc";
+import { useRouter } from "next/router";
+
+const Home: NextPage = () => {
+  const { status } = useSession();
+  const router = useRouter();
+  if (status === "unauthenticated") router.replace("api/auth/signin");
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        </h1>
+        <div className="flex flex-col items-center gap-2">
+          <AuthShowcase />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => signOut() : () => signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
